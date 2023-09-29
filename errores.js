7a8158016360c295301256c0f2e740107b464279
MarioMozo <mario.psy@live.com>
2023-09-29 14:16:41 -0600

    try and catch, errores

---
+++ errores.js
@@ -0,0 +1,14 @@
+const { log } = require("console");
+
+function seRompe(){
+    return 4 + i;
+}
+
+try{
+    reRompe();
+    
+}catch(err){
+    console.log('Algo se rompio');
+    console.error(err.message);
+    console.log('No pasa nada, lo hemos atrapado');
+}
