47160178c968a58f5beb8b279ff4f1797b807abd
MarioMozo <mario.psy@live.com>
2023-10-02 12:24:25 -0600

    Proceso hijo en practica

--- procesoHijo.js
+++ procesoHijo.js
@@ -1,11 +1,15 @@
 // import { exec } from 'node:child_process';
 // import { stdout } from 'node:process';
 
-const {exec} = require('child_process');
-exec('ls -a', (exec, stdout, sterr) => {
-    if(err){
-        console.error(err);
-        return false;
-    }
-    console.log(stdout);
-})
+const {exec, spawn} = require('child_process');
+// exec('dir', (err, stdout, sterr) => {
+//     if(err){
+//         console.error(err);
+//         return false;
+//     }
+//     console.log(stdout);
+// })
+
+let proceso = spawn('ls');
+console.log(proceso.pid);
+console.log(proceso.connected);
