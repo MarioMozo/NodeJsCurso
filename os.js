db6ad5fb61e0de99dc0f7cef61abeebf06baf65e
MarioMozo <mario.psy@live.com>
2023-10-02 13:23:23 -0600

    Funcion OS, libreria de bajo nivel para el sistema operativo

---
+++ os.js
@@ -0,0 +1,25 @@
+const os = require('os');
+
+console.log(os.platform());    //Te indica que tipo de plataforma (os) estas corriendo
+
+console.log(os.arch());
+
+
+function procesador() {
+    let CPU = os.cpus();
+
+    for(i = 0; i<=CPU.length; i++){
+        console.log('Procesador:', i);
+    }    
+    
+}
+procesador();
+
+
+console.log(os.freemem());
+
+console.log(os.homedir());
+
+console.log(os.hostname());
+
+console.log(os.networkInterfaces());
